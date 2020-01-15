20c528b20dd53b0925dc1318fca9641a43ac7fe8
Hans Polders <hans.polders@gmail.com>
2020-01-15 19:34:57 +0100

    add team selector

---
+++ Firmware/Constants.h
@@ -0,0 +1,17 @@
+/* Pin's */
+const byte WS2812_Pin = 10;
+const byte TeamSelect_R_Pin = 2;
+const byte TeamSelect_G_Pin = 3;
+const byte TeamSelect_B_Pin = 4;
+
+
+
+const byte WS2812_Leds = 9;
+
+const byte Rex = 0;
+const byte Giggle = 1;
+const byte Buzz = 2;
+
+const uint32_t RedRGB = 0x00FF0000;
+const uint32_t GreenRGB = 0x0000FF00;
+const uint32_t BlueRGB = 0x000000FF;
--- Firmware/Firmware.ino
+++ Firmware/Firmware.ino
@@ -1,9 +1,49 @@
-void setup() {
-  // put your setup code here, to run once:
+/*
+  Firmware for the Fri3d Camp Timeblaster 2020 Rev. 00
+*/
+#include <Adafruit_NeoPixel.h> //https://github.com/adafruit/Adafruit_NeoPixel
+#include "Constants.h"
 
+Adafruit_NeoPixel strip(WS2812_Leds, WS2812_Pin, NEO_RGB + NEO_KHZ800);
+
+void setup() 
+{
+  pinMode(TeamSelect_R_Pin, INPUT_PULLUP);
+  pinMode(TeamSelect_G_Pin, INPUT_PULLUP);
+  pinMode(TeamSelect_B_Pin, INPUT_PULLUP);
+
+  strip.begin();
+  strip.setPixelColor(0, strip.Color(128, 128, 128));
+  strip.show();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  byte selectedTeam;
+  selectedTeam = readTeamSelector();
+  setTeamLED(selectedTeam);
+}
 
+void setTeamLED(byte team)
+{
+  uint32_t teamColor;
+  switch (team) {
+    case Rex:
+      teamColor = RedRGB;
+      break;
+    case Giggle:
+      teamColor = GreenRGB;
+      break;
+    case Buzz:
+      teamColor = BlueRGB;
+      break;
+  }
+  strip.setPixelColor(0, teamColor);
+  strip.show();
+}
+
+byte readTeamSelector()
+{
+  if (!digitalRead(TeamSelect_R_Pin)) return Rex;
+  if (!digitalRead(TeamSelect_G_Pin)) return Giggle;
+  return Buzz;
 }
